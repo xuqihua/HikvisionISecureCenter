ef4d3f7eb2cc8023e37cd7d5d1b02f299e2f60fd
xuqihua <xuqihua@lixiang.com>
2022-02-09 09:12:53 +0800

    update composer

--- demo.php
+++ demo.php
@@ -1,13 +1,13 @@
 <?php
-
+include './HikvisionISecureCenter.php'
 
 $h      = new HikvisionISecureCenter("https://192.168.1.1", "app_key", "app_secret");
 $token  = '';
 $params = ['pageNo' => 1, 'pageSize' => 100];
 # 取token  用于大批量操作，但是一定要确保token没有过期，自行处理token过期与重新获取
-#var_dump($h->getToken());
+var_dump($h->getToken());
 # 取人员列表，第三个参数可不传 使用签名
-#var_dump($h->doRequest('/api/resource/v1/person/personList', $params, $token));
+var_dump($h->doRequest('/api/resource/v1/person/personList', $params, $token));
 
 # 取根组织 没有传token
 var_dump($h->doRequest('/api/resource/v1/org/rootOrg', []));
